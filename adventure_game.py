2bddd33a774042c0ffd56ea095f56da7e48ed0e0
FinchyG <lagaunt120@yahoo.com>
2018-10-05 20:08:42 +0100

    initial commit

---
+++ adventure_game.py
@@ -0,0 +1,7 @@
+def adventure_game():
+
+    print("You stand on an cracked path at a fork before an old oak forest.")
+    entry = input("Will you go left or right?")
+    print("You chose %s"%(entry))
+
+adventure_game()
